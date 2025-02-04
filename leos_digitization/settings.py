0ccdff32663a5932cab4ce3fb2a0e7ba360bb3f0
krishna <krishnacse_vce@hotmail.com>
2025-02-04 22:42:32 +0530

    made project level static directory with files

--- leos_digitization/settings.py
+++ leos_digitization/settings.py
@@ -112,6 +112,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# Add this section to include the common static directory
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
