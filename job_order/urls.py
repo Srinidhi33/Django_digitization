e6d5e9a543e79c4145af40972cc03a8013a2f36b
krishna09 <krishnacse_vce@hotmail.com>
2025-02-05 14:05:15 +0530

    Job_request_html_added

--- job_order/urls.py
+++ job_order/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="job-order-home")
+    path("", views.home, name="job-order-home"),
+    path("request/", views.job_request, name="job-request")
 ]
--- job_order/views.py
+++ job_order/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     
     return render(request, 'job_order/home.html')
+
+def job_request(request):
+    
+    return render(request, 'job_order/job_request.html')
